de0412e8803b177d4ddb7308097fead301e8f796
Sayuri <sayuri48@hawaii.edu>
2023-09-15 10:56:26 -1000

    Completed Part 5 Problem Sets

--- If else statement part 1_9.12.23.playground/Contents.swift
+++ If else statement part 1_9.12.23.playground/Contents.swift
@@ -26,9 +26,63 @@ var gradeA = true
 if gradeA == true
 {
     print("I really like class")
-    
 }
+
 else
 {
     print("I'm not so sure about coding in Swift")
+}
+
+
+//Part 5 Problem Set 1
+var intOne = 1
+var intTwo = 2
+
+if intOne > intTwo
+{
+    print("coffee")
+}
+    
+else
+{
+    print("tea")
+}
+
+
+//Part 5 Problem Set 2
+var stringOne = "Meow"
+var stringTwo = "Meow"
+
+if stringOne == stringTwo
+{
+    print(stringOne + stringTwo)
+}
+    
+
+//Part 5 Problem Set 3
+var touchFloor1 = true
+var touchFloor2 = true
+
+touchFloor1 != true
+touchFloor2 == false
+
+if touchFloor1 != true
+{
+    print("move the floor1")
+}
+    
+else
+{
+    print("stop the floor1")
+}
+
+
+if touchFloor2 == false
+{
+    print("die")
+}
+    
+else
+{
+    print("alive")
 }
